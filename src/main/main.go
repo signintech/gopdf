9200c14f30450e312470e2d7b2d1e395c978f3a6
oneplus1000 <oneplus1000@gmail.com>
2013-02-26 12:14:12 +0700

    ...

--- src/main/main.go
+++ src/main/main.go
@@ -11,14 +11,15 @@ func main() {
 
 	fmt.Println("start...")
 	pdf := gopdf.GoPdf{}
-	pdf.Start(gopdf.Config{Unit: "pt", PageSize: gopdf.Rect{W: 595.28, H: 841.89}}) //595.28, 841.89 A4
+	pdf.Start(gopdf.Config{Unit: "pt", PageSize: gopdf.Rect{W: 595.28, H: 841.89}}) //595.28, 841.89 = A4
 	pdf.AddFont("THSarabunPSK",new(fonts.THSarabun),"res/fonts/THSarabun.z")
 	pdf.AddFont("Loma",new(fonts.Loma),"res/fonts/Loma.z")
 	pdf.AddPage()
 	pdf.SetFont("THSarabunPSK", "B", 14)
-	pdf.Cell(nil,  ToCp874("กAโจตลาด  2 ล้อ พุ่ง 20% รับปีใหม่ คาดเอที toyota ยังแรงกุ้งตั้ว "))
+	pdf.Cell(nil,  ToCp874("ทดสอบ"))
+	pdf.Cell(nil,  ToCp874("Test"))
 	pdf.Br(28)
-	pdf.SetFont("THSarabunPSK", "B", 26)
+	/*pdf.SetFont("THSarabunPSK", "B", 26)
 	pdf.Image("/data/CODES/WORKS/GoPdf_ws/GoPdf/test/me.jpg",20,10,nil)
 	pdf.Cell(nil,  ToCp874("ด้วยการที่เราไม่ไปทำยังไง"))
 	pdf.Cell(nil,  ToCp874("ศาลอาญา ไม่ชี้ชัด ใครยิง “ มานะ อาจราญ” เจ้าหน้าที่สวนสัตว์ดุสิตเสียชีวิต"))
@@ -33,7 +34,7 @@ func main() {
 	pdf.Cell(&gopdf.Rect{H: 100, W: 100}, ToCp874("การบ้านx"))
 	pdf.Cell(&gopdf.Rect{H: 100, W: 100}, ToCp874("การบ้านx") )
 	pdf.SetLineWidth(1)
-	pdf.Line(10,10,500,10)
+	pdf.Line(10,10,500,10)*/
 	
 	pdf.WritePdf("/var/www/fpdf17/output/x.pdf")
 	fmt.Println("end...")

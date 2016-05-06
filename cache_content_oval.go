5bbb239330e499167eabf2c2e6b89b019888691e
oneplus1000 <oneplus1000@gmail.com>
2016-05-06 15:37:10 +0700

    change AppendStreamOval use cacheContent

---
+++ cache_content_oval.go
@@ -0,0 +1,50 @@
+package gopdf
+
+import (
+	"bytes"
+	"fmt"
+)
+
+type cacheContentOval struct {
+	pageHeight float64
+	x1         float64
+	y1         float64
+	x2         float64
+	y2         float64
+}
+
+func (c *cacheContentOval) toStream() (*bytes.Buffer, error) {
+
+	h := c.pageHeight
+	x1 := c.x1
+	y1 := c.y1
+	x2 := c.x2
+	y2 := c.y2
+
+	var buff bytes.Buffer
+	cp := 0.55228                              // Magnification of the control point
+	v1 := [2]float64{x1 + (x2-x1)/2, h - y2}   // Vertex of the lower
+	v2 := [2]float64{x2, h - (y1 + (y2-y1)/2)} // .. Right
+	v3 := [2]float64{x1 + (x2-x1)/2, h - y1}   // .. Upper
+	v4 := [2]float64{x1, h - (y1 + (y2-y1)/2)} // .. Left
+
+	buff.WriteString(fmt.Sprintf("%0.2f %0.2f m\n", v1[0], v1[1]))
+	buff.WriteString(fmt.Sprintf(
+		"%0.2f %0.2f %0.2f %0.2f %0.2f %0.2f c\n",
+		v1[0]+(x2-x1)/2*cp, v1[1], v2[0], v2[1]-(y2-y1)/2*cp, v2[0], v2[1],
+	))
+	buff.WriteString(fmt.Sprintf(
+		"%0.2f %0.2f %0.2f %0.2f %0.2f %0.2f c\n",
+		v2[0], v2[1]+(y2-y1)/2*cp, v3[0]+(x2-x1)/2*cp, v3[1], v3[0], v3[1],
+	))
+	buff.WriteString(fmt.Sprintf(
+		"%0.2f %0.2f %0.2f %0.2f %0.2f %0.2f c\n",
+		v3[0]-(x2-x1)/2*cp, v3[1], v4[0], v4[1]+(y2-y1)/2*cp, v4[0], v4[1],
+	))
+	buff.WriteString(fmt.Sprintf(
+		"%0.2f %0.2f %0.2f %0.2f %0.2f %0.2f c S\n",
+		v4[0], v4[1]-(y2-y1)/2*cp, v1[0]-(x2-x1)/2*cp, v1[1], v1[0], v1[1],
+	))
+
+	return &buff, nil
+}
--- content_obj.go
+++ content_obj.go
@@ -138,30 +138,13 @@ func (c *ContentObj) AppendStreamRectangle(x float64, y float64, wdth float64, h
 
 //AppendStreamOval append oval
 func (c *ContentObj) AppendStreamOval(x1 float64, y1 float64, x2 float64, y2 float64) {
-	h := c.getRoot().config.PageSize.H
-	cp := 0.55228                              // Magnification of the control point
-	v1 := [2]float64{x1 + (x2-x1)/2, h - y2}   // Vertex of the lower
-	v2 := [2]float64{x2, h - (y1 + (y2-y1)/2)} // .. Right
-	v3 := [2]float64{x1 + (x2-x1)/2, h - y1}   // .. Upper
-	v4 := [2]float64{x1, h - (y1 + (y2-y1)/2)} // .. Left
-
-	c.stream.WriteString(fmt.Sprintf("%0.2f %0.2f m\n", v1[0], v1[1]))
-	c.stream.WriteString(fmt.Sprintf(
-		"%0.2f %0.2f %0.2f %0.2f %0.2f %0.2f c\n",
-		v1[0]+(x2-x1)/2*cp, v1[1], v2[0], v2[1]-(y2-y1)/2*cp, v2[0], v2[1],
-	))
-	c.stream.WriteString(fmt.Sprintf(
-		"%0.2f %0.2f %0.2f %0.2f %0.2f %0.2f c\n",
-		v2[0], v2[1]+(y2-y1)/2*cp, v3[0]+(x2-x1)/2*cp, v3[1], v3[0], v3[1],
-	))
-	c.stream.WriteString(fmt.Sprintf(
-		"%0.2f %0.2f %0.2f %0.2f %0.2f %0.2f c\n",
-		v3[0]-(x2-x1)/2*cp, v3[1], v4[0], v4[1]+(y2-y1)/2*cp, v4[0], v4[1],
-	))
-	c.stream.WriteString(fmt.Sprintf(
-		"%0.2f %0.2f %0.2f %0.2f %0.2f %0.2f c S\n",
-		v4[0], v4[1]-(y2-y1)/2*cp, v1[0]-(x2-x1)/2*cp, v1[1], v1[0], v1[1],
-	))
+	var cache cacheContentOval
+	cache.pageHeight = c.getRoot().config.PageSize.H
+	cache.x1 = x1
+	cache.y1 = y1
+	cache.x2 = x2
+	cache.y2 = y2
+	c.listCache.append(&cache)
 }
 
 //AppendStreamCurve draw curve

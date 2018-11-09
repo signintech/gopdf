50e930ba3e582205963d1443026c77738e629e4d
ykwmz@me.com <wyj@vico-lab.com>
2018-11-09 18:33:32 +0800

    When Draw Line, No Need Close Path, so use S NOT s

--- cache_content_line.go
+++ cache_content_line.go
@@ -14,6 +14,6 @@ type cacheContentLine struct {
 }
 
 func (c *cacheContentLine) write(w io.Writer, protection *PDFProtection) error {
-	fmt.Fprintf(w, "%0.2f %0.2f m %0.2f %0.2f l s\n", c.x1, c.pageHeight-c.y1, c.x2, c.pageHeight-c.y2)
+	fmt.Fprintf(w, "%0.2f %0.2f m %0.2f %0.2f l S\n", c.x1, c.pageHeight-c.y1, c.x2, c.pageHeight-c.y2)
 	return nil
 }

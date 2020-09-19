bed826e540262f4e7a6391ad2f59eb1454673cbc
DanAr1404 <45997134+DanAr1404@users.noreply.github.com>
2020-09-19 18:57:48 +0300

    added allborders parameter

--- cell_option.go
+++ cell_option.go
@@ -12,12 +12,12 @@ const Bottom = 1 //000001
 const Center = 16 //010000
 //Middle middle
 const Middle = 32 //100000
-//All all
-const ALL = 15 //001111
+//AllBorders allborders
+const AllBorders = 15 //001111
 
 //CellOption cell option
 type CellOption struct {
 	Align  int //Allows to align the text. Possible values are: Left,Center,Right,Top,Bottom,Middle
-	Border int //Indicates if borders must be drawn around the cell. Possible values are: Left, Top, Right, Bottom, ALL
+	Border int //Indicates if borders must be drawn around the cell. Possible values are: Left, Top, Right, Bottom, AllBorders
 	Float  int //Indicates where the current position should go after the call. Possible values are: Right, Bottom
 }

31694277e6e962517beb2fa036d6161f5b9fa76e
oneplus1000 <oneplus1000@gmail.com>
2020-11-01 11:49:02 +0700

    parse 2.1 Multiple Substitution Format 1

--- fontmaker/core/gsub_lookup_subtable_type2.go
+++ fontmaker/core/gsub_lookup_subtable_type2.go
@@ -3,9 +3,20 @@ package core
 //GSUBLookupSubTableType2Format1 -> 2.1 Multiple Substitution Format 1
 type GSUBLookupSubTableType2Format1 struct {
 	substFormat     uint
-	coverageOffset  int64                     //Offset to Coverage table, from beginning of substitution subtable
-	sequenceCount   uint                      //Number of Sequence table offsets in the sequenceOffsets array
-	sequenceOffsets []GSUBLookupSequenceTable //Array of offsets to Sequence tables. Offsets are from beginning of substitution subtable, ordered by Coverage index
+	coverageOffset  int64   //Offset to Coverage table, from beginning of substitution subtable
+	sequenceCount   uint    //Number of Sequence table offsets in the sequenceOffsets array
+	sequenceOffsets []int64 //Array of offsets to Sequence tables. Offsets are from beginning of substitution subtable, ordered by Coverage index
+	sequenceTable   []GSUBLookupSequenceTable
+}
+
+//LookupType get lookup type
+func (g GSUBLookupSubTableType2Format1) LookupType() uint {
+	return 2
+}
+
+//Format get format
+func (g GSUBLookupSubTableType2Format1) Format() uint {
+	return 1
 }
 
 //GSUBLookupSequenceTable Sequence table
--- fontmaker/core/ttfparser_gsub_subtable_type2.go
+++ fontmaker/core/ttfparser_gsub_subtable_type2.go
@@ -1,6 +1,8 @@
 package core
 
-import "bytes"
+import (
+	"bytes"
+)
 
 //2.1 Multiple Substitution Format 1
 func (t *TTFParser) parseGSUBLookupListTableSubTableLookupType2Format1(
@@ -9,11 +11,60 @@ func (t *TTFParser) parseGSUBLookupListTableSubTableLookupType2Format1(
 	substFormat uint,
 	gdefResult ParseGDEFResult,
 ) (
-	GSUBLookupSubTableType1Format1,
+	GSUBLookupSubTableType2Format1,
 	error,
 ) {
+	result := GSUBLookupSubTableType2Format1{}
 
-	result := GSUBLookupSubTableType1Format1{}
+	coverageOffset, err := t.ReadUShort(fd)
+	if err != nil {
+		return GSUBLookupSubTableType2Format1{}, err
+	}
+	result.coverageOffset = int64(coverageOffset) + offset //set result
+
+	sequenceCount, err := t.ReadUShort(fd)
+	if err != nil {
+		return GSUBLookupSubTableType2Format1{}, err
+	}
+	result.sequenceCount = sequenceCount //set result
+
+	var sequenceOffsets []int64
+	for x := uint(0); x < sequenceCount; x++ {
+		sequenceOffset, err := t.ReadUShort(fd)
+		if err != nil {
+			return GSUBLookupSubTableType2Format1{}, err
+		}
+		sequenceOffsets = append(sequenceOffsets, int64(sequenceOffset)+offset)
+	}
+	result.sequenceOffsets = sequenceOffsets //set result
+
+	var sequenceTables []GSUBLookupSequenceTable
+	for _, offset := range sequenceOffsets {
+
+		_, err := fd.Seek(offset, 0)
+		if err != nil {
+			return GSUBLookupSubTableType2Format1{}, err
+		}
+		glyphCount, err := t.ReadUShort(fd)
+		if err != nil {
+			return GSUBLookupSubTableType2Format1{}, err
+		}
+		var substituteGlyphIDs []uint
+		for y := uint(0); y < glyphCount; y++ {
+			substituteGlyphID, err := t.ReadUShort(fd)
+			if err != nil {
+				return GSUBLookupSubTableType2Format1{}, err
+			}
+			substituteGlyphIDs = append(substituteGlyphIDs, substituteGlyphID)
+		}
+
+		sequenceTables = append(sequenceTables, GSUBLookupSequenceTable{
+			glyphCount:         glyphCount,
+			substituteGlyphIDs: substituteGlyphIDs,
+		})
+	}
+
+	result.sequenceTable = sequenceTables
 
 	return result, nil
 }

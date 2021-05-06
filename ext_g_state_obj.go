9c2fdb21ec8e8f183bc281c972997f988bebf687
Aleksandr Khylobokyi <a_khll@yahoo.com>
2021-05-06 13:52:23 +0300

    to test

--- ext_g_state_obj.go
+++ ext_g_state_obj.go
@@ -3,7 +3,6 @@ package gopdf
 import (
 	"fmt"
 	"io"
-	"strings"
 	"sync"
 
 	"github.com/pkg/errors"
@@ -12,17 +11,17 @@ import (
 // TODO: add all fields https://www.adobe.com/content/dam/acom/en/devnet/acrobat/pdfs/PDF32000_2008.pdf 8.4.5 page 128
 type ExtGState struct {
 	Index      int
-	ca         float64
-	CA         float64
-	BM         string
-	SMaskIndex int
+	ca         *float64
+	CA         *float64
+	BM         *BlendModeType
+	SMaskIndex *int
 }
 
 type ExtGStateOptions struct {
 	StrokingCA    *float64
 	NonStrokingCa *float64
-	BlendMode     *string
-	SMaskOptions  *SMaskOptions
+	BlendMode     *BlendModeType
+	SMaskIndex    *int
 }
 
 func (extOpt ExtGStateOptions) GetId() string {
@@ -36,67 +35,36 @@ func (extOpt ExtGStateOptions) GetId() string {
 	if extOpt.BlendMode != nil {
 		id += fmt.Sprintf("BM_%s;", *extOpt.BlendMode)
 	}
-
-	if extOpt.SMaskOptions != nil {
-		maskImgs := make([]string, len(extOpt.SMaskOptions.Images))
-		for ind, maskImg := range extOpt.SMaskOptions.Images {
-			maskImgs[ind] = fmt.Sprintf("%d", maskImg.index)
-		}
-
-		if len(maskImgs) > 0 {
-			maskKey := "maskImgs_" + strings.Join(maskImgs, ",") + ";"
-			id += maskKey
-		}
-
-		exgStateIndexes := make([]string, len(extOpt.SMaskOptions.ExtGStateIndexes))
-		for ind, exgStateIndex := range extOpt.SMaskOptions.ExtGStateIndexes {
-			exgStateIndexes[ind] = fmt.Sprintf("%d", exgStateIndex)
-		}
-
-		if len(exgStateIndexes) > 0 {
-			exgStateKey := "exgState_" + strings.Join(exgStateIndexes, ",") + ";"
-			id += exgStateKey
-		}
+	if extOpt.SMaskIndex != nil {
+		id += fmt.Sprintf("SMask_%d_0_R;", *extOpt.SMaskIndex)
 	}
 
 	return id
 }
 
-func NewExtGState(opts ExtGStateOptions, gp *GoPdf) (ExtGState, error) {
-	state := ExtGState{}
-	if opts.BlendMode != nil {
-		state.BM = *opts.BlendMode
-	}
-	if opts.StrokingCA != nil {
-		state.CA = *opts.StrokingCA
-	}
-	if opts.NonStrokingCa != nil {
-		state.ca = *opts.NonStrokingCa
-	}
-
-	extGState, ok := gp.curr.extGStateMap.Find(opts)
+func GetCachedExtGState(opts ExtGStateOptions, gp *GoPdf) (ExtGState, error) {
+	extGState, ok := gp.curr.extGStatesMap.Find(opts)
 	if !ok {
-		if opts.SMaskOptions != nil {
-			smask, err := NewSMask(*opts.SMaskOptions, gp)
-			if err != nil {
-				return ExtGState{}, err
-			}
-
-			state.SMaskIndex = smask.Index
+		extGState = ExtGState{
+			BM:         opts.BlendMode,
+			CA:         opts.StrokingCA,
+			ca:         opts.NonStrokingCa,
+			SMaskIndex: opts.SMaskIndex,
 		}
 
-		state.Index = gp.addObj(state)
+		extGState.Index = gp.addObj(extGState)
 
 		pdfObj := gp.pdfObjs[gp.indexOfProcSet]
 		procset, ok := pdfObj.(*ProcSetObj)
 		if !ok {
 			return ExtGState{}, errors.New("can't convert pdfobject to procsetobj")
 		}
-		procset.ExtGStates = append(procset.ExtGStates, ExtGS{Index: state.Index})
 
-		gp.curr.extGStateMap.Save(opts.GetId(), state)
+		procset.ExtGStates = append(procset.ExtGStates, ExtGS{Index: extGState.Index})
 
-		extGState = state
+		gp.curr.extGStatesMap.Save(opts.GetId(), extGState)
+
+		extGState = extGState
 	}
 
 	return extGState, nil
@@ -111,12 +79,19 @@ func (egs ExtGState) getType() string {
 func (egs ExtGState) write(w io.Writer, objID int) error {
 	content := "<<\n"
 	content += "\t/Type /ExtGState\n"
-	content += fmt.Sprintf("\t/ca %.3F\n", egs.ca)
-	content += fmt.Sprintf("\t/CA %.3F\n", egs.CA)
-	content += fmt.Sprintf("\t/BM %s\n", egs.BM)
 
-	if egs.SMaskIndex != 0 {
-		content += fmt.Sprintf("\t/SMask %d 0 R\n", egs.SMaskIndex+1)
+	if egs.ca != nil {
+		content += fmt.Sprintf("\t/ca %.3F\n", *egs.ca)
+	}
+	if egs.CA != nil {
+		content += fmt.Sprintf("\t/CA %.3F\n", *egs.CA)
+	}
+	if egs.BM != nil {
+		content += fmt.Sprintf("\t/BM %s\n", *egs.BM)
+	}
+
+	if egs.SMaskIndex != nil {
+		content += fmt.Sprintf("\t/SMask %d 0 R\n", *egs.SMaskIndex+1)
 	}
 
 	content += ">>\n"
@@ -128,19 +103,19 @@ func (egs ExtGState) write(w io.Writer, objID int) error {
 	return nil
 }
 
-type ExtGStateMap struct {
+type ExtGStatesMap struct {
 	syncer sync.Mutex
 	table  map[string]ExtGState
 }
 
-func NewExtGStateMap() ExtGStateMap {
-	return ExtGStateMap{
+func NewExtGStatesMap() ExtGStatesMap {
+	return ExtGStatesMap{
 		syncer: sync.Mutex{},
 		table:  make(map[string]ExtGState),
 	}
 }
 
-func (extm *ExtGStateMap) Find(extGState ExtGStateOptions) (ExtGState, bool) {
+func (extm *ExtGStatesMap) Find(extGState ExtGStateOptions) (ExtGState, bool) {
 	key := extGState.GetId()
 
 	extm.syncer.Lock()
@@ -155,7 +130,7 @@ func (extm *ExtGStateMap) Find(extGState ExtGStateOptions) (ExtGState, bool) {
 
 }
 
-func (tm *ExtGStateMap) Save(id string, extGState ExtGState) ExtGState {
+func (tm *ExtGStatesMap) Save(id string, extGState ExtGState) ExtGState {
 	tm.syncer.Lock()
 	defer tm.syncer.Unlock()
 

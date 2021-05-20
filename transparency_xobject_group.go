0db38f9c7382fb0b85eea6ee18a1a3042fb82777
Aleksandr Khylobokyi <a_khll@yahoo.com>
2021-05-20 16:36:24 +0300

    get image and extgstate refs from parent

--- transparency_xobject_group.go
+++ transparency_xobject_group.go
@@ -71,25 +71,6 @@ func (s TransparencyXObjectGroup) write(w io.Writer, objId int) error {
 	content += fmt.Sprintf("\t/Matrix [1 0 0 1 0 0]\n")
 	content += fmt.Sprintf("\t/BBox [%.3F %.3F %.3F %.3F]\n", s.BBox[0], s.BBox[1], s.BBox[2], s.BBox[3])
 	content += "\t/Group<</CS /DeviceGray /S /Transparency>>\n"
-	content += "\t/Resources<<\n"
-	content += "\t\t/ProcSet [/PDF]\n"
-
-	xObjects := "\t\t/XObject<<\n"
-	for _, XObject := range s.XObjects {
-		xObjects += fmt.Sprintf("\t\t\t/I%d %d 0 R\n", XObject.index+1, XObject.index+1)
-	}
-	xObjects += "\t\t>>\n"
-
-	extGStates := "\t\t/ExtGState<<\n"
-	for _, extGStateIndex := range s.ExtGStateIndexes {
-		extGStates += fmt.Sprintf("\t\t\t/GS%d %d 0 R\n", extGStateIndex+1, extGStateIndex)
-	}
-	extGStates += "\t\t>>\n"
-
-	content += xObjects
-	content += extGStates
-
-	content += "\t>>\n"
 
 	content += fmt.Sprintf("\t/Length %d\n", len(streamBuff.Bytes()))
 	content += ">>\n"
